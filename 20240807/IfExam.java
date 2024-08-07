df280a0a79b91e98722ea3ae886ea8e66ae013f8
Simkm-98 <simkm980@gmail.com>
2024-08-07 10:08:02 +0900

    Add files via upload
    0807 자바 실습 예제

---
+++ 20240807/IfExam.java
@@ -0,0 +1,70 @@
+package day02;
+import java.util.Scanner;
+
+public class IfExam {
+
+	public static void main(String[] args) {
+		int num = 0;
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		String hakjum = "";
+		
+	/*	switch 문
+		switch (num) {
+		case 95 : case 96 : case 97 : case 98 : case 99 : case 100 :
+			hakjum = "A+";
+			break;
+		case 90 : case 91 : case 92 : case 93 : case 94 :
+			hakjum = "A0";
+			break;
+		case 85 : case 86 : case 87 : case 88 : case 89 :
+			hakjum = "B+";
+			break;
+		case 80 : case 81 : case 82 : case 83 : case 84 :
+			hakjum = "B0";
+			break;
+		case 75 : case 76 : case 77 : case 78 : case 79 :
+			hakjum = "C+";
+			break;
+		case 70 : case 71 : case 72 : case 73 : case 74 :
+			hakjum = "C0";
+			break;
+		case 65 : case 66 : case 67 : case 68 : case 69 :
+			hakjum = "D+";
+			break;
+		case 60 : case 61 : case 62 : case 63 : case 64 :
+			hakjum = "D0";
+			break;
+		default :
+			hakjum = "F";
+			break;
+		}
+	
+ 		if 문	
+		if (num >= 95) {
+			hakjum = "A+";
+		}else if (num >= 90) {
+			hakjum = "A0";
+		}else if (num >= 85) {
+			hakjum = "B+";
+		}else if (num >= 80) {
+			hakjum = "B0";
+		}else if (num >= 75) {
+			hakjum = "C+";
+		}else if (num >= 70) {
+			hakjum = "C0";
+		}else if (num >= 65) {
+			hakjum = "D+";
+		}else if (num >= 60) {
+			hakjum = "D0";
+		}else if (num < 60) {
+			hakjum = "F";
+		}else 
+			System.out.println("숫자만 입력해 주세요");
+		System.out.println("입력한 점수 : " + num + " 학점은 : " + hakjum);
+	*/
+
+	} 
+	
+}	
